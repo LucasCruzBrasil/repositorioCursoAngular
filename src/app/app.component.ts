56c140d8d520ed127eef88336bd94ad897d71ea2
LucasCruzBrasil <lucaszaratustra@gmail.com>
2021-01-26 06:48:18 -0300

    gitgnore file

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ nome = 'Lucas';
+ adicionado = false;
+ 
+ adicionar() {
+  console.log(`Adicionando ${this.nome}`);
+this.adicionado = true;
+  
+}
+
+ 
+
+}
+
+
+
